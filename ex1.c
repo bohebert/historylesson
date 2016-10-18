b4a614c80e884edb833de2cc298c720d32537c74
Bo <Hebert@Bos-MacBook-Pro.local>
2016-10-18 11:50:03 -0700

    First commit

---
+++ ex1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/* This is the end */
+int main(int argc, char *argv[])
+{
+	int distance = 2654;
+	int times = 4;
+	int one = 1;
+	int no = 0;
+	 
+	
+	// this is also the end 
+	printf("You are %d miles away from home.\n",distance);
+	printf("You have overdosed %d times this year.\n",times);
+	printf("You only have %d chance at this.\n",one);
+	printf("You have %d excuses left.\n",no);
+	
+			return 0;
+}	
